0e0cc3b26967de72cd848852c0e409b40666b407
falkheiland <falkheiland@users.noreply.github.com>
2020-11-01 15:53:23 +0100

    Update 01-Demo.ps1

--- PSIGEL/Demo/01-Demo.ps1
+++ PSIGEL/Demo/01-Demo.ps1
@@ -1,88 +1,54 @@
-#region Start Windows Terminal
-
-wt -p "Windows | Windows PowerShell (5.1)" `; new-tab -p "Windows | PowerShell 7.0.3 (Core)" `; new-tab -p "Ubuntu |  PowerShell 7.0.3 (Core)" `; focus-tab -t 0
-
+#region Import Module PSIGEL
+Import-Module -FullyQualifiedName C:\GitHub\PSIGEL\PSIGEL\PSIGEL.psd1
 #endregion
 
-#region Set Parameter
-
-$DSC = [IO.Path]::DirectorySeparatorChar
-$PSIGELPath = 'GitHub{0}PSIGEL{0}PSIGEL{0}PSIGEL.psd1' -f $DSC
-
+#region set parameters
+$CredPath = 'C:\Credentials\UmsRmdb.cred'
 $PSDefaultParameterValues = @{
   '*-UMS*:Computername' = 'igelrmserver'
-  '*-UMS*:TCPPort'      = 9443
-  '*-UMS*:Confirm'      = $False
+  '*-UMS*:TCPPort'      = 9443 #Default 8443, here NAT to VirtualBox
 }
+#endregion
 
-if (($PSEdition -eq 'Core' -and $IsWindows) -or ($PSEdition -eq 'Desktop' -and ($PSVersionTable.PSVersion.Major -eq 5 -and $PSVersionTable.PSVersion.Minor -eq 1)))
-{
-  # PS7 on Windows or Windows PowerShell 5.1
-  Import-Module -FullyQualifiedName ('C:\{0}' -f $PSIGELPath) -Force
-  $PSDefaultParameterValues.Add('New-UMSAPICookie:Credential', (Import-Clixml -Path 'C:\Credentials\UmsRmdb.cred'))
-}
-elseif ($PSEdition -eq 'core' -and (-Not $IsWindows) )
-{
-  # PS7 on Linux OR MacOS
-  Import-Module -FullyQualifiedName ('/mnt/c/{0}' -f $PSIGELPath) -Force
-  # Dont use the following method in production, since on linux the clixml file is not encrypted
-  $PSDefaultParameterValues.Add('New-UMSAPICookie:Credential', (Import-Clixml -Path '/mnt/c/Credentials/UmsRmdbWsl.cred'))
-}
-
-$WebSession = New-UMSAPICookie
+#region create a websession
+$WebSession = New-UMSAPICookie -Credential (Import-Clixml -Path $CredPath)
 $PSDefaultParameterValues.Add('*-UMS*:WebSession', $WebSession)
-
 #endregion
 
-#region Get-UMSStatus
-
-$UMSStatus = Get-UMSStatus
-$UMSStatus
-
-#endregion
-
-#region Get-Firmware
-
+#region get all firmwares
 $FirmwareColl = Get-UMSFirmware
 $FirmwareColl
-
 #endregion
 
-#region Get latest Firmeware
-
+#region get latest firmware
 $LatestFirmware = $FirmwareColl | Sort-Object -Property Version -Descending |
   Select-Object -First 1
 $LatestFirmware
-
 #endregion
 
-#region Set Comment "Update" on device with older firmwares
-
-$CommentUpdateColl = (Get-UMSDevice).where{ $_.FirmwareId -ne $LatestFirmware.Id } |
-  Update-UMSDevice -Comment 'Update'
-$CommentUpdateColl
-
+#region remove a comment "update" from all devices with the latest firmware
+$null = Get-UMSDevice -Filter details | Where-Object {
+  $_.Comment -eq 'update' -and $_.FirmwareId -eq $LatestFirmware.Id
+} | Update-UMSDevice -Comment ''
 #endregion
 
-#region Get online Device information on Devicees with Comment "Update"
-
-$DeviceOnlineColl = $CommentUpdateColl | Get-UMSDevice -Filter online
-$DeviceOnlineColl
-
-#endregion
-
-#region Get Departments with device that have comment Comment "Update"
-
-$DeviceDirectoryColl = Get-UMSDeviceDirectory
-$DepartmentUpdateColl = ($DeviceDirectoryColl).Where{
-  $_.ID -in $DeviceOnlineColl.ParentId
+#region get all online devices that do not have the latest firmware
+# because the devices here are non bon existing, $false is used for online detection
+$UpdateDeviceColl = Get-UMSDevice -Filter online | Where-Object {
+  $_.Online -eq $false -and $_.FirmwareId -ne $LatestFirmware.Id
 }
-$DepartmentUpdateColl
-
+$UpdateDeviceColl
 #endregion
 
-#region Remove-UMSAPICookie
+#region set a comment "update" to all devices with not the latest firmware
+$UpdateDeviceColl | Update-UMSDevice -Comment 'update'
+#endregion
 
-$null = Remove-UMSAPICookie -WebSession $WebSession
+#region remove the used websession
+$null = Remove-UMSAPICookie
+#endregion
 
+#region create view and job
+promptly after the execution of this script, a scheduled job "update on Shutdown"
+on a view for all devices with the comment "update" should start in the UMS.
 #endregion
